d6503e81965961a765a5b2e5397d2d945bc21a61
benhar-dev <53424421+benhar-dev@users.noreply.github.com>
2024-11-11 15:54:46 +1100

    updated autoupdate

--- src/node.js/public/scripts/fetch-rpc-api-connection.js
+++ src/node.js/public/scripts/fetch-rpc-api-connection.js
@@ -1,21 +1,22 @@
 export class ApiConnection {
   async send(methodName, params) {
-    const response = await fetch(`/rpc/${methodName}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(params),
-    });
+    try {
+      const response = await fetch(`/rpc/${methodName}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(params),
+      });
 
-    if (!response.ok) {
-      const errorBody = await response.text(); // Fetching text instead of json to avoid errors in case of non-JSON responses
-      console.log(
-        `RPC call failed with status ${response.status}: ${errorBody}`
-      );
-      throw new Error(`RPC call failed: ${response.status}: ${errorBody}`);
+      if (!response.ok) {
+        const errorBody = await response.text(); // Fetching text instead of json to avoid errors in case of non-JSON responses
+        throw new Error(`RPC call failed: ${response.status}: ${errorBody}`);
+      }
+
+      return await response.json();
+    } catch (error) {
+      throw error;
     }
-
-    return await response.json();
   }
 }
--- src/node.js/public/scripts/index.js
+++ src/node.js/public/scripts/index.js
@@ -4,14 +4,13 @@ const { IecDatatypesPack } = MobjectGraphUiIecDatatypesPack;
 
 import { ApiConnection } from "./fetch-rpc-api-connection.js";
 
-var graphFramework = new GraphFramework();
+const graphFramework = new GraphFramework();
 graphFramework.install(new DefaultPack());
 graphFramework.install(IecDatatypesPack); // todo : change pack to be new
 graphFramework.install(VisionPack); // todo : change pack to be new
 
-var apiConnection = new ApiConnection();
-
-new GraphEditor("my-editor", apiConnection);
+const apiConnection = new ApiConnection();
+const graphEditor = new GraphEditor("my-editor", apiConnection);
 
 document
   .getElementById("getBlueprintsBtn")
